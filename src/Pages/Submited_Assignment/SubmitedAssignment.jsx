96881c51f633c15c1ad40ac107696ea5d202af39
123naim <mdnayemiqbal886@gmail.com>
2023-11-18 11:15:05 +0600

    submites assignment completed

--- src/Pages/Submited_Assignment/SubmitedAssignment.jsx
+++ src/Pages/Submited_Assignment/SubmitedAssignment.jsx
@@ -13,11 +13,14 @@ const SubmitedAssignment = () => {
             })
     }, [])
     return (
-        <div className="flex justify-center items-center">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 lg:grid-cols-3">
-                {
-                    allData.map(data => <SubmitedData key={data._id} data={data}></SubmitedData>)
-                }
+        <div>
+            <h2 className="text-3xl font-bold text-center mb-8 text-black">Submited Assignment</h2>
+            <div className="flex justify-center items-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 lg:grid-cols-3">
+                    {
+                        allData.map(data => <SubmitedData key={data._id} data={data}></SubmitedData>)
+                    }
+                </div>
             </div>
         </div>
     );
